86ed30c727903caf4e3fb4fd2e1e4b123d2bf565
Andrew Sidorchuk <sidorchukandrew@gmail.com>
2025-09-29 17:45:34 -0400

    add swiftui toggle

--- example/App.tsx
+++ example/App.tsx
@@ -1,73 +1,18 @@
-import { useEvent } from 'expo';
-import AutoSizingView, { AutoSizingView } from 'auto-sizing-view';
-import { Button, SafeAreaView, ScrollView, Text, View } from 'react-native';
+import { AutoSizingView } from 'auto-sizing-view';
+import { StyleSheet, View } from 'react-native';
 
 export default function App() {
-  const onChangePayload = useEvent(AutoSizingView, 'onChange');
-
   return (
-    <SafeAreaView style={styles.container}>
-      <ScrollView style={styles.container}>
-        <Text style={styles.header}>Module API Example</Text>
-        <Group name="Constants">
-          <Text>{AutoSizingView.PI}</Text>
-        </Group>
-        <Group name="Functions">
-          <Text>{AutoSizingView.hello()}</Text>
-        </Group>
-        <Group name="Async functions">
-          <Button
-            title="Set value"
-            onPress={async () => {
-              await AutoSizingView.setValueAsync('Hello from JS!');
-            }}
-          />
-        </Group>
-        <Group name="Events">
-          <Text>{onChangePayload?.value}</Text>
-        </Group>
-        <Group name="Views">
-          <AutoSizingView
-            url="https://www.example.com"
-            onLoad={({ nativeEvent: { url } }) => console.log(`Loaded: ${url}`)}
-            style={styles.view}
-          />
-        </Group>
-      </ScrollView>
-    </SafeAreaView>
-  );
-}
-
-function Group(props: { name: string; children: React.ReactNode }) {
-  return (
-    <View style={styles.group}>
-      <Text style={styles.groupHeader}>{props.name}</Text>
-      {props.children}
+    <View style={styles.container}>
+      <AutoSizingView style={{ backgroundColor: 'red' }} />
     </View>
   );
 }
 
-const styles = {
-  header: {
-    fontSize: 30,
-    margin: 20,
-  },
-  groupHeader: {
-    fontSize: 20,
-    marginBottom: 20,
-  },
-  group: {
-    margin: 20,
-    backgroundColor: '#fff',
-    borderRadius: 10,
-    padding: 20,
-  },
+const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#eee',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
-  view: {
-    flex: 1,
-    height: 200,
-  },
-};
+});
--- src/AutoSizingView.tsx
+++ src/AutoSizingView.tsx
@@ -1,11 +1,8 @@
 import { requireNativeView } from 'expo';
 import * as React from 'react';
 
-import { AutoSizingViewProps } from './AutoSizingView.types';
+const NativeView: React.ComponentType = requireNativeView('AutoSizingView');
 
-const NativeView: React.ComponentType<AutoSizingViewProps> =
-  requireNativeView('AutoSizingView');
-
-export default function AutoSizingView(props: AutoSizingViewProps) {
+export default function AutoSizingView(props: any) {
   return <NativeView {...props} />;
 }
