24e24b0ac4b8dd3bcf736e9ac95307c6d4b98b6e
nishan (o^▽^o) <nishanbende@gmail.com>
2025-10-01 09:54:25 +0530

    use setStyleSize call

--- example/App.tsx
+++ example/App.tsx
@@ -1,13 +1,10 @@
 import { AutoSizingView } from "auto-sizing-view";
 import { StyleSheet, View } from "react-native";
-import { Host } from "@expo/ui/swift-ui";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Host matchContents style={{ backgroundColor: "red" }}>
-        <AutoSizingView />
-      </Host>
+      <AutoSizingView style={{ backgroundColor: "red" }} />
     </View>
   );
 }
--- ios/AutoSizingView.swift
+++ ios/AutoSizingView.swift
@@ -1,16 +1,35 @@
 import ExpoModulesCore
 import SwiftUI
 
-struct AutoSizingView: ExpoSwiftUI.View  {
-    @State var checked: Bool = false
+struct AutoSizingView: ExpoSwiftUI.View, ExpoSwiftUI.WithHostingView  {
+  @State var checked: Bool = false
+  @EnvironmentObject var shadowNodeProxy: ExpoSwiftUI.ShadowNodeProxy
+
+  init(props: ExpoModulesCore.ExpoSwiftUIDefaultProps) {}
     
-    init(props: ExpoModulesCore.ExpoSwiftUIDefaultProps) {
-        
-    }
+  private func setShadowNodeSize(width: CGFloat, height: CGFloat) {
+    shadowNodeProxy.setStyleSize?(NSNumber(value: width), NSNumber(value: height))
+  }
+  
+  var body: some View {
+    let toggleBody = Toggle("", isOn: $checked)
+      .labelsHidden()
+      .toggleStyle(.switch)
     
-    var body: some View {
-        Toggle("", isOn: $checked)
-            .labelsHidden()
-            .toggleStyle(.switch)
+    if #available(iOS 16.0, *) {
+      toggleBody.onGeometryChange(for: CGSize.self, of: { proxy in proxy.size }, action: { value in
+        setShadowNodeSize(width: value.width, height: value.height)
+      })
+    } else {
+      toggleBody.overlay {
+        GeometryReader { geometry in
+          Color.clear
+            .hidden()
+            .onAppear {
+              setShadowNodeSize(width: geometry.size.width, height: geometry.size.height)
+            }
+        }
+      }
     }
+  }
 }
